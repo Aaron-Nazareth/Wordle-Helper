dad802a156d55085aa55d89a3b3a4030106edc89
zhengyang qiu <15224003720@163.com>
2022-03-21 18:02:24 +0000

    get first word

--- wordle-helper_client_side/src/Components/WordList.js
+++ wordle-helper_client_side/src/Components/WordList.js
@@ -1,15 +1,23 @@
 import Word from "./Word";
 
+//the name of wordlist is we still wanna return wordlist, but to find first word
+
+
 const WordList = ({ words }) => {
 
     const wordNodes = words.map(word => {
         return <Word word={word} key={word.id} />
     })
+    //take the array the first .map is to find the first word
+    //.at() will take integer index and return the value
+    //we can put logic here
+    const firstWord = wordNodes.at(0);
 
 
     return (
         <>
-            {wordNodes}
+            {/* {wordNodes} */}
+            {firstWord}
         </>
     )
 
--- wordle-helper_client_side/src/Containers/WordleContainer.js
+++ wordle-helper_client_side/src/Containers/WordleContainer.js
@@ -8,23 +8,35 @@ import WordList from "../Components/WordList";
 //setting our state
 const WordleContainer = () => {
 
-    const [words, setWords] = useState([])
+    const [words, setWords] = useState([]);
+    // const [guessWord,setGuessWord]=useState("");
+    // let url = `http://localhost:8080/helper/start/${guessWord}`;
+
 
     useEffect(() => {
+        
         fetch("http://localhost:8080/helper/start")
+        //do not need to be state
+        // fetch("http://localhost:8080/helper/start/{words}");
+       
+
+
+        //update variable in the path
             .then(response => response.json())
             .then(data => setWords(data))
     }, [])
-
-
+    
 
     return (
         <>
             <WordList words={words} />
+            
         </>
     )
 
 }
+
+
 
 
 export default WordleContainer;
