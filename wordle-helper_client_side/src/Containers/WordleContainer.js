28ff63cd457655865fb810e7c58910fbc51cb875
Aaron Nazareth <aaronnazareth@hotmail.co.uk>
2022-03-23 14:30:15 +0000

    Added comments to explain our code

--- wordle-helper_client_side/src/Containers/WordleContainer.js
+++ wordle-helper_client_side/src/Containers/WordleContainer.js
@@ -10,26 +10,24 @@ import SubmitForm from "../Components/SubmitForm";
 const WordleContainer = () => {
 
     const [words, setWords] = useState([]);
-    // const [guessWord,setGuessWord]=useState("");
-    // let url = `http://localhost:8080/helper/start/${guessWord}`;
 
 
     useEffect(() => {
         
         fetch("http://localhost:8080/helper/start")
-        //does not need to be state
             .then(response => response.json())  // Strip data we want from response using json function
             .then(data => setWords(data[0]))    // Set words array to be the first index of our data
-        
-        
-        // fetch("http://localhost:8080/helper/start/{words}");
-        //update variable in the path
     }, [])
 
+    
+    // fetch("http://localhost:8080/helper/start/{words}");
+    //update variable in the path
     const filterWordList = async (newWordObject) => {
-            console.log(words.word);
-            console.log(newWordObject);
-            const newWords = await fetch(`http://localhost:8080/helper/start/${words.word}`, {
+            // console.log(words.word);
+            // console.log(newWordObject);
+
+            // Need to use an await below because we don't want the function to execute until the fetch request is satisfied - account for even the smallest fraction of a delay in response, since working with a large database
+            await fetch(`http://localhost:8080/helper/start/${words.word}`, {
             method: "DELETE",
             headers: {
                 'Content-Type': 'application/json'
@@ -37,11 +35,11 @@ const WordleContainer = () => {
             body: JSON.stringify(newWordObject)
         })
             .then(response => response.json())  // Strip data we want from our response
-            // .then(data => console.log(data[0])) // Update state for words array with first index filtered word that is new suggestion
-            console.log(newWords);
-            setWords(newWords[0]);
-            console.log("these are your new word");
-            console.log(words);
+            .then(data => setWords(data[0])) // Update state for words array with first index filtered word that is new suggestion
+            // console.log(newWords);
+            // setWords(newWords[0]); - alternative use, which requires "newWords" variable to be created - have "const newWords = " before the await fetch
+            // console.log("these are your new word");
+            // console.log(words);
         }
     
 
