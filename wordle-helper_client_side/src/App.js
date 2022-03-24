65f504088eae6a8864c935247a6d5f31d8cbe3c0
zhengyang qiu <15224003720@163.com>
2022-03-24 15:53:16 +0000

    add pop up window

--- wordle-helper_client_side/src/App.js
+++ wordle-helper_client_side/src/App.js
@@ -3,13 +3,22 @@ import './App.css';
 import WordleContainer from './Containers/WordleContainer';
 import word2 from './images/word2.png';
 import NavigationBar from './Components/NavigationBar';
+import Popup from './Components/PopUp';
+
+
 
 function App() {
+
+
   return (
     <>
-        <NavigationBar/>
+
+   
+        <NavigationBar />
         <img src={word2} className="App-logo" alt="wordle logo" />
         <WordleContainer/>
+        {/* <Popup/> */}
+      
     </>
 
   )
