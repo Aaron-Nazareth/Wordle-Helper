0b926d65dd2c583416178ade64de9bb1bb585652
Aaron Nazareth <aaronnazareth@hotmail.co.uk>
2022-03-24 22:09:55 +0000

    Turning our colour changing functionality back on

--- wordle-helper_client_side/src/Components/Grid.js
+++ wordle-helper_client_side/src/Components/Grid.js
@@ -24,25 +24,25 @@ useEffect(()=>{
 
 
 
-// const items = document.getElementsByClassName("item")
-//  const itemColour = ["grey", "yellow", "green"]
+const items = document.getElementsByClassName("item")
+ const itemColour = ["grey", "yellow", "green"]
 
-// for (let i = 0; i < items.length; i++){
-//   items[i].addEventListener("click", ()  => {
+for (let i = 0; i < items.length; i++){
+  items[i].addEventListener("click", ()  => {
 
-//   let item = items[i]
+  let item = items[i]
 
-//     const currentColour = item.getAttribute("colour")
+    const currentColour = item.getAttribute("colour")
 
  
-//     const colourIndex = itemColour.indexOf(currentColour);
+    const colourIndex = itemColour.indexOf(currentColour);
  
-//     const nextColour = itemColour[(colourIndex + 1)%3];
+    const nextColour = itemColour[(colourIndex + 1)%3];
  
-//     item.setAttribute("colour", nextColour)
+    item.setAttribute("colour", nextColour)
  
-//   })
-// }
+  })
+}
 
 
 
